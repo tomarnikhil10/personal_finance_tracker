ecdabf98b41596c76a5c4e28017348f461fa0433
tomarnikhil10 <138971786+tomarnikhil10@users.noreply.github.com>
2025-04-07 15:21:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "personal_finance_tracker";
+$host = 'dpg-cvppr615pdvs73edep50-a.oregon-postgres.render.com';
+$db   = 'personal_finance_db_1wd9';
+$user = 'personal_finance_db_1wd9_user';
+$pass = 'qtULMnZrMU4qUYtt5axR6z91oxTv0uWM';
+$port = '5432';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected to PostgreSQL successfully!";
+} catch (PDOException $e) {
+    die("PostgreSQL connection failed: " . $e->getMessage());
 }
 ?>
